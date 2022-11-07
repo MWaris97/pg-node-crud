4d023e74240247730d15339886a596adb664ae11
Muhammad Waris <16cs44@students.muet.edu.pk>
2022-11-07 12:19:24 +0500

    remove cred

--- dbConn.js
+++ dbConn.js
@@ -1,13 +1,4 @@
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'raanxvubehwrtu',
-  host: 'ec2-52-204-157-26.compute-1.amazonaws.com',
-  database: 'da9fq9qair8e0h',
-  password: '69bd2cd57f2d7edd108d0f7c9831b8bacf5d994015bfb70ef9d8878d4d706660',
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = new Pool();
 
 export default pool;
