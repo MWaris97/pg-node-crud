7d3a6cad4c29e4a25a12a7ee6389340bd484c7af
Muhammad Waris <16cs44@students.muet.edu.pk>
2022-11-10 09:41:26 +0500

    Create todo task API done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const db = require('./queries')
 const port = 3000
+
+const pool = require('./dbConn')
 
 app.use(bodyParser.json())
 app.use(
@@ -15,11 +16,29 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get('/testdb', async (request, response) => {
+  let res = await pool.query('select * from public.todoList')
+  response.json({
+    todo: res.rows
+  })
+})
+
+app.post('/todo/create', async (req, res) => {
+  let result = await pool.query(`INSERT INTO public.todolist
+  (id, task, done)
+  VALUES($1, $2, $3)`, 
+  [req.body.id, req.body.task, req.body.done])
+  console.log(result);
+  res.json({
+    "status": "Task created"
+  })
+})
+
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
